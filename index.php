6d40cf6bd57c81904833fc2e75d0a2552e6f8b18
agusbektir <agusbektir@gmail.com>
2020-06-23 20:39:52 +0700

    add paragraph to file index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote - Dari Local ke Github</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas excepturi et sint perspiciatis quisquam est magni repudiandae, laudantium odit illum dolor aspernatur, delectus nihil nisi in rem provident, mollitia natus!</p>
+</body>
+</html>
